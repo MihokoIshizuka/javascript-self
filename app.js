ca32954f2e836f00c1bc9eff1e84548907b9ff05
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-05-11 22:16:57 +0900

    [addclass]

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 $(function () {
   $('.box1').mouseover(function() {
-    $('.box1').css({'background-color':'#0000FF'});
+    $('.box1').addClass('box1-ext');
+  }).mouseout(function () {
+    $('.box1').removeClass('box1-ext');
   });
 });
